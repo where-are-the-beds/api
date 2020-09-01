3e75c8c9698f0998fd0e98add6d3142a221a4f7d
alanaalfeche <alanaalfeche@gmail.com>
2020-08-31 23:40:12 -0500

    Add Bed Utilization Rate View

--- mapper.py
+++ mapper.py
@@ -1,21 +1,35 @@
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
-import folium
-from folium import Circle, Marker
+from folium import Map, Circle, Marker, Popup
 
 
 def get_beds():
-    df = pd.read_csv('usa-hospital-beds.csv')
-
-    m_1 = folium.Map(location=[36.778259, -119.417931], tiles='cartodbpositron', zoom_start=10)
-
-    m_2 = folium.Map(location=[36.778259, -119.417931], tiles='cartodbpositron', zoom_start=10)
-
-    df = df.dropna()
-
-    # Marker -- Find all available beds
+    df = get_df()
+    _map = Map(location=[41.8781, -87.6298], tiles='cartodbpositron', zoom_start=10)
     for idx, row in df.iterrows():
-        Marker([row['Y'], row['X']], popup=row['HOSPITAL_NAME']).add_to(m_1)
+        Marker([row['Y'], row['X']], popup=row['HOSPITAL_NAME']).add_to(_map)
+    _map.save('templates/all_beds.html')
 
-    m_1.save('templates/all_beds.html')
+def get_beds_utilization_rate():
+    df = get_df()
+    _map = Map(location=[41.8781, -87.6298], tiles='cartodbpositron', zoom_start=10)
+    for i in range(0,len(df)):
+        Circle(
+            location=[df.iloc[i]['Y'], df.iloc[i]['X']],
+            radius=10000,
+            fill=True,
+            color=color(df.iloc[i]['BED_UTILIZATION'])).add_to(_map)
+    _map.save('templates/bed_utilization_rate.html')
+
+def color(val):
+    if val >= 0.7:
+        return 'forestgreen'
+    elif val >= 0.4:
+        return 'blue'
+    else:
+        return 'darkred'
+
+def get_df():
+    df = pd.read_csv('usa-hospital-beds.csv')
+    return df.dropna()
--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import csv
 import os.path
 
 from s3 import list_bucket, download_file
-from mapper import get_beds
+from mapper import get_beds, get_beds_utilization_rate
 
 app = Flask(__name__)
 
@@ -24,6 +24,13 @@ def beds():
     get_beds()
     return render_template('all_beds.html') 
 
+@app.route('/beds-utilization-rate')
+def beds_utilization_rate():
+    CSV_PATH = './usa-hospital-beds.csv'
+    if not os.path.exists(CSV_PATH):
+        download_file()
+    get_beds_utilization_rate()
+    return render_template('bed_utilization_rate.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
